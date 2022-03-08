6bf8cacd6ce7a5882d905dc467dc7773f033e49a
joZecodes <zachary.r.dom@gmail.com>
2022-03-08 08:13:56 -0700

    set up welcome route 9.5.2

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
 import datetime as dt
 import numpy as np
 import pandas as pd
+import datetime as dt
+import numpy as np
+import pandas as pd
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
 
 engine = create_engine("sqlite:///hawaii.sqlite")
-
 Base = automap_base()
-Base.prepare(engine, reflect=True)
+Base.prepare(engine,reflect=True)
 
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 session = Session(engine)
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return(
+        f"Welcome to the Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start/end"
+    )
