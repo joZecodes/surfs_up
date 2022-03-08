8c9fc2de6764e29ee6f6675dadd8179fd6bba420
joZecodes <zachary.r.dom@gmail.com>
2022-03-07 20:54:12 -0700

    created database for app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
-app = Flask(__name__)
+engine = create_engine("sqlite:///hawaii.sqlite")
 
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+Base = automap_base()
+Base.prepare(engine, reflect=True)
 
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
